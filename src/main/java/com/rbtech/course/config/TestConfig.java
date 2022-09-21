29a7d47eb1e80f3365b4bea91c9b2a94ddf418d5
Roberto Bruno <robertobcb@gmail.com>
2022-09-21 13:24:21 -0300

    Payment, one-to-one association

--- src/main/java/com/rbtech/course/config/TestConfig.java
+++ src/main/java/com/rbtech/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.rbtech.course.antities.Category;
 import com.rbtech.course.antities.Order;
 import com.rbtech.course.antities.OrderItem;
+import com.rbtech.course.antities.Payment;
 import com.rbtech.course.antities.Product;
 import com.rbtech.course.antities.User;
 import com.rbtech.course.antities.enums.OrderStatus;
@@ -80,6 +81,12 @@ public class TestConfig  implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+		
 	}
 	
 	
